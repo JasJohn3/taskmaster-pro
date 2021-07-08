89f9a1cd924f7da7d9b5f7e11f92e265c6df9cc8
Your Name <jasjohn3@uat.edu>
2021-07-08 09:24:18 -0700

    drag-drop feature implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,70 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger('focus');
 
 });
+// Create Sortable UL Elements
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  over: function(event){
+    console.log("over", this);
+  },
+  out : function(event){
+    console.log("out", this);
+  },
+  update: function(event){
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+  
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+  
+    console.log(text, date);
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      // trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+    });
+  }
+});
+
+// Create a recycle bin on the page for tasks
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 // Task editing textarea comes out of focus
 $(".list-group").on('blur','textarea',function (){
   // get the textarea's current value/text
